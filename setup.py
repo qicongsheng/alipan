023ebcc0eeae537e33985f2ac2c19c479f7f6ae9
875881559@qq.com <Best12167>
2022-12-14 17:52:38 +0800

    阿里网盘

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ from setuptools import setup, find_packages
 from alipan import help
 
 setup(
-    name = 'alipan',
-    version = help.get_version(),
+    name='alipan',
+    version=help.get_version(),
     keywords='alipan',
-    description = 'Commandline tools for aliyundrive',
-    license = 'MIT License',
-    url = 'https://github.com/qicongsheng/alipan',
-    author = 'qicongsheng',
-    author_email = 'qicongsheng@outlook.com',
-    packages = find_packages(),
-    include_package_data = True,
-    platforms = 'any',
-    install_requires = [
+    description='Commandline tools for aliyundrive',
+    license='MIT License',
+    url='https://github.com/qicongsheng/alipan',
+    author='qicongsheng',
+    author_email='qicongsheng@outlook.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
         'aligo==3.5.1'
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'alipan = alipan.__main__:main'
         ]
